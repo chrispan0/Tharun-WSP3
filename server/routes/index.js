b5ba8f07ba76e575ae917f232ad03a643014f285
Tharun-nav <tharun.naveen@ontariotechu.net>
2024-11-24 06:01:38 -0500

    update 3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/index', function(req, res, next) {
+  res.render('home', { title: 'Index' });
+});
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
