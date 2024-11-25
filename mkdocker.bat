a7ed9a98f4c6416cd0492dbe75ce6bfcc0b35be2
Chris Panetta <cv.panetta@gmail.com>
2024-11-24 21:20:06 -0500

    docker stuff

--- mkdocker.bat
+++ mkdocker.bat
@@ -1,7 +1,8 @@
 git fetch
 git pull
-docker stop wsp-assignment-3
-docker rm wsp-assignment-3
-cd C:\Users\Administrator\Documents\GitHub\WSP-Assignment-3\WSP-Assignment-3
-docker build -t wsp-assignment-3 .
-docker run -d -p 1447:4000 --name wsp-assignment-3 wsp-assignment-3:latest
+docker stop tharun-a3
+docker rm tharun-a3
+cd C:\Users\Administrator\Documents\GitHub\Tharun-WSP3
+docker build -t tharun-a3 .
+docker run -d -p 1437:3000 --name tharun-a3 tharun-a3:latest
+pause
